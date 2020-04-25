bcbad187612bc3057e66cd3c5c0997f99957ce50
mw <mw>
2020-04-25 14:16:59 +0200

    loguj/wyloguj

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,14 @@
 <template>
     <div>
-        <h1>Twój e-mail to: {{ email }}</h1>
+        <h1>Witaj w systemie zapisów na zajęcia</h1>
+		<div v-if="authrnticatedUsername != ''">
+		<button@click="logMeOut()"> wyloguj </button>
+		</div>
 
-        <div v-if="email.length < 10">Ale masz krótki adres!</div>
-        <div v-else-if="email.length < 15">Twój adres e-mail jest w sam raz.</div>
-        <div v-else>Twój adres e-mail jest stanowczo za długi.</div>
-
+<div v-else>
         <input type="email" v-model="email">
-		<button@click="alertMyEmail()"> wyświetl mail w alercie </button>
+		<button@click="logMeIn()"> zaloguj </button>
+		</div>
     </div>
 </template>
 
@@ -15,13 +16,18 @@
     export default {
         data() {
             return {
-                email: ''
+                email: '',
+				authrnticatedUsername: ''
             }
         },
 		
 		methods: {
-			alertMyEmail() {
-			alert(this.email);
+			logMeIn() {
+			this.authrnticatedUsername = this.email;
+			},
+			
+			logMeOut() {
+			this.authrnticatedUsername = '';
 			}
 		
 		}
